eb40257e9a98e767e69857e3b65f2cfc0344efc8
Tikava <wavoleniu@gmail.com>
2024-05-14 02:39:22 +0500

    Copied pages folder from branch master

---
+++ src/pages/new/New.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+
+const New = () => {
+  const [file, setFile] = useState("");
+  const [formData, setFormData] = useState({
+    house: "",
+    address: "",
+    price: "",
+    rating: "",
+    comment: ""
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const sendHouse = (e) => {
+    e.preventDefault();
+    console.log(formData); // Log the form data
+    // Reset the form
+    setFile("");
+    setFormData({
+      house: "",
+      address: "",
+      price: "",
+      rating: "",
+      comment: ""
+    });
+  };
+
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add new House</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              <div className="formInput">
+                <label>House:</label>
+                <input
+                  type="text"
+                  name="house"
+                  value={formData.house}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="formInput">
+                <label>Address:</label>
+                <input
+                  type="text"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="formInput">
+                <label>Price:</label>
+                <input
+                  type="text"
+                  name="price"
+                  value={formData.price}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="formInput">
+                <label>Rating:</label>
+                <input
+                  type="text"
+                  name="rating"
+                  value={formData.rating}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="formInput">
+                <label>Comment:</label>
+                <input
+                  type="text"
+                  name="comment"
+                  value={formData.comment}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <button onClick={sendHouse}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
